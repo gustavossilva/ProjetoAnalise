54ce336bd2c66675b20822931d0f155a54cfb185
Gustavo Silva <xgustavossilva@gmail.com>
2017-06-11 16:15:55 -0300

    Commit inicial com o projeto

---
+++ cmake-build-debug/CMakeFiles/ProjetoAnalise.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ProjetoAnalise.dir/main.c.o"
+  "ProjetoAnalise.pdb"
+  "ProjetoAnalise"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ProjetoAnalise.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
