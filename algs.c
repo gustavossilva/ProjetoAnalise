e574dd3c229000181931a5c43e9d80270c28ec8c
Guilherme Souza <guilherme.souza.silva95@gmail.com>
2017-06-12 14:28:48 -0300

    merge sort and insertion sort ok

--- algs.c
+++ algs.c
@@ -3,61 +3,58 @@
 //
 #include <stdio.h>
 #include <math.h>
+#include <stdlib.h>
+void insertion(int * v);
+void ordena_intercala(int * v,int p,int r);
+void intercala(int * v,int p, int q, int r);
 
-
-//int main() {
-//    int v[10] = {1,7,5,654,12,3,48,7,5,6};
-//    int p = 10/2;
-//    int r = 10;
-//    ordena_intercala(v,p,r);
-//}
-
-void ordena_intercala(int * v,int p,int r)
-{
-    if (p < r) {
-        int q = floor((p + r) / 2); // retorna o chão dessa operação
-        ordena_intercala (v, p, q);
-        ordena_intercala(v, q + 1, r);
-        intercala(v, p, q, r);
-    }
-}
-void intercala(int * v,int p, int q, int r)
-{
-    int B[10];
-    int n = 10;
-    int i,k,j;
-    q = n/2;
-    r = n;
-    p = 0;
-    for (i = p; i<q; i++)
-        B[i] = v[i];
-    for (j = (q + 1); j< r;j++)
-        B[(r + q + 1 - j)] = v[j];
-    i = p;
-    j = r;
-    for(k = p; k<r; k++) {
-        if (B[i] <= B[j])
-        {
-            v[k] = B[i];
-            i = (i+1);
-        }
-        else
-        {
-            v[k] = B[j];
-            j = (j-1);
-        }
-
-    }
-
-    for(i = 0; i<10;i++)
+int main() {
+    int v[10] = {1,2,3,4,7,8,9,5,15,654};
+    int p = 0;
+    int r = 9;
+    ordena_intercala(v,0,r);
+//    insertion(v);
+    for(int i =0; i<= r; i++)
     {
         printf("%d ",v[i]);
     }
 }
 
-void insertion()
+void ordena_intercala(int * v,int p,int r)
 {
-    int v[10] = {1,7,5,654,12,3,48,7,5,6};
+    int q;
+    if (p < r) {
+        q = floor((p + r) / 2); // retorna o chão dessa operação
+        ordena_intercala (v, p, q);
+        ordena_intercala(v, q + 1, r);
+        intercala(v, p, q, r);
+    }
+}
+
+void intercala(int * v,int p, int q, int r)
+{
+    int B[10];
+    int i,k,j;
+    for (i = p; i<=q; i++)
+        B[i] = v[i];
+    for (j = (q + 1); j<= r;j++) {
+        B[(r + q + 1 - j)] = v[j];
+    }
+    i = p;
+    j = r;
+    for(k = p; k<=r; k++) {
+            if (B[i] <= B[j]) {
+                v[k] = B[i];
+                i++;
+            } else {
+                v[k] = B[j];
+                j--;
+            }
+    }
+}
+
+void insertion(int *v)
+{
     int chave,i,j;
     for(j=2;j<10;j++)
     {
@@ -69,9 +66,5 @@ void insertion()
             i = i-1;
             v[i+1] = chave;
         }
-    }
-    for(i = 0; i<10;i++)
-    {
-        printf("%d ",v[i]);
     }
 }
